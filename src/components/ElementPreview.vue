acdf20870bf00d99850ea08d2d6c16bd51d6ffe4
Caleb Foss <calebfoss@gmail.com>
2023-05-14 18:47:33 -0500

    Add type for element ref

--- src/components/ElementPreview.vue
+++ src/components/ElementPreview.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue'
 
-const { element } = defineProps<{ element: MarkerElement; }>();
-const elRef = ref<any | null>(null);
-const isCanvas = element.tag === "canvas";
+const { element } = defineProps<{ element: MarkerElement }>()
+const elRef = ref<(HTMLElement & { runCode: () => void }) | null>(null)
+const isCanvas = element.tag === 'canvas'
 
-onMounted(async () => {
-  if (elRef.value === null) return;
-  const el = elRef.value;
+onMounted(() => {
+  if (elRef.value === null) return
+  const el = elRef.value
 
-  for (const [name, value] of Object.entries(element.attributes))
-    el.setAttribute(name, value);
+  for (const [name, value] of Object.entries(element.attributes)) el.setAttribute(name, value)
 
-  if (element.tag === 'canvas')
-    el.runCode();
-});
-
-
+  if (element.tag === 'canvas') el.runCode()
+})
 </script>
 
 <template>
-  <component :is="isCanvas ? 'canvas' : `p-${element.tag}`" v-bind="isCanvas ? { is: 'p-canvas' } : null" ref="elRef">
+  <component
+    :is="isCanvas ? 'canvas' : `p-${element.tag}`"
+    v-bind="isCanvas ? { is: 'p-canvas' } : null"
+    ref="elRef"
+  >
     <ElementPreview v-for="child in element.children" :element="child"></ElementPreview>
   </component>
 </template>
