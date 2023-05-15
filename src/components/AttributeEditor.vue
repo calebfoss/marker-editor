50a8ade835e8380264beee95be3de65d2e573bd0
Caleb Foss <calebfoss@gmail.com>
2023-05-14 20:05:33 -0500

    Use v-model for input value This resolves an issue with the canvas not updating the first time

--- src/components/AttributeEditor.vue
+++ src/components/AttributeEditor.vue
@@ -6,28 +6,13 @@ const { name, allAttributes } = defineProps<{
   remove: (name: string) => void
   refreshPreview: () => void
 }>()
-
-const expression = computed({
-  get() {
-    return allAttributes[name]
-  },
-  set(val) {
-    allAttributes[name] = val
-  }
-})
-
-function handleChange(e: Event) {
-  const target = e.target as HTMLInputElement
-  expression.value = target.value
-}
 </script>
 
 <template>
   <div class="attribute-editor" :tabindex="0" @keydown.delete.stop="(e) => remove(name)">
     <label>{{ name }}</label>
     <input
-      :value="expression"
-      @change="handleChange"
+      v-model="allAttributes[name]"
       @keydown.delete.stop
       @keydown.enter="refreshPreview"
       @blur="refreshPreview"
